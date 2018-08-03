000ee151a07788f4d7a995f3bd518fc838b9fda8
Marc Abramowitz <abramowi@adobe.com>
2018-08-03 09:38:40 -0700

    Remove wiki link in comment
    in case we open source

--- cmd/hyperion-cli/kubernetes_auth.go
+++ cmd/hyperion-cli/kubernetes_auth.go
@@ -2,5 +2,4 @@ package main
 
 // The following import seems to be necessary in order for me to access the
 // Ethos Kubernetes integration cluster in Azure
-// https://wiki.corp.adobe.com/display/CoreServicesTeam/Ethos+Kubernetes+Integration+Cluster
 import _ "k8s.io/client-go/plugin/pkg/client/auth"
